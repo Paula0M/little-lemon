169c3a7185c7ba95818b88be67fc5ff57682622f
Paula0M <paulamirazf@gmail.com>
2025-01-26 14:54:31 +0100

    Special Image

--- src/Specials.js
+++ src/Specials.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function Specials() {
   const specials = [
-    { id: 1, name: 'Special 1', description: 'Description 1', price: '$10' },
-    { id: 2, name: 'Special 2', description: 'Description 2', price: '$15' },
-    { id: 3, name: 'Special 3', description: 'Description 3', price: '$20' },
+    { id: 1, name: 'Special 1', description: 'Description 1', price: '$10', image: 'special1.jpg' },
+    { id: 2, name: 'Special 2', description: 'Description 2', price: '$15', image: 'special2.jpg' },
+    { id: 3, name: 'Special 3', description: 'Description 3', price: '$20', image: 'special3.jpg' },
   ];
 
   return (
@@ -14,6 +14,7 @@ function Specials() {
       <div className="specials-list">
         {specials.map(special => (
           <div key={special.id} className="special-item">
+            <img src={`${process.env.PUBLIC_URL}/${special.image}`} alt={special.name} className="special-image" />
             <h3>{special.name}</h3>
             <p>{special.description}</p>
             <span>{special.price}</span>
