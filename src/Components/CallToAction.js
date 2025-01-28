286551666313384c5651e36ed07840d670ce3d1b
Paula0M <paulamirazf@gmail.com>
2025-01-28 18:07:58 +0100

    header imagen

--- src/Components/CallToAction.js
+++ src/Components/CallToAction.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import '../App.css';
-import  img from '../assets/restauranfood1.png';
+import img from '../Assets/restauranfood1.png';
 
 function CallToAction() {
   return (
     <section className="call-to-action">
+      <div className='cta-background'>
       <div className="cta-content">
-        <h2>Little Lemon</h2>
-        <h3>Chicago</h3>
-        <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-        <button>Reserve a Table</button>
+        <div className="cta-content-text">
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          <button>Reserve a Table</button>
+        </div>
+        <div className="cta-image">
+          <img src={img} alt="Restaurant Food" />
+        </div>
       </div>
-      <div className="cta-image">
-      <img src={img} alt="Restaurant1" className="cta-image" />
       </div>
     </section>
-  )
+  );
 }
 
 export default CallToAction;
