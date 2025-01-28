886be2922d12f4f4cfb20690abc18648abd2a094
Paula0M <paulamirazf@gmail.com>
2025-01-28 17:08:15 +0100

    Imsage 1

--- src/Components/CallToAction.js
+++ src/Components/CallToAction.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import '../App.css';
+import  img from '../assets/restauranfood1.png';
 
 function CallToAction() {
   return (
     <section className="call-to-action">
-      <h1>Welcome to Little Lemon</h1>
-      <p>Experience the best dining at Little Lemon. Enjoy our delicious meals and warm atmosphere. Perfect for family and friends.</p>
-      <button>Book a Table</button>
+      <div className="cta-content">
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+        <button>Reserve a Table</button>
+      </div>
+      <div className="cta-image">
+      <img src={img} alt="Restaurant1" className="cta-image" />
+      </div>
     </section>
-  );
+  )
 }
 
 export default CallToAction;
