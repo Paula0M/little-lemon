191cc0ec1630ae8de5c05ff89b8d39b6c550e89c
Paula0M <paulamirazf@gmail.com>
2025-01-30 13:08:04 +0100

    Chicago align

--- src/Components/Chicago.js
+++ src/Components/Chicago.js
@@ -4,9 +4,11 @@ import '../App.css';
 function Chicago() {
   return (
     <section className="chicago">
-      <h2>Little Lemon</h2>
-      <h3>Chicago</h3>
-      <p>Little Lemon is a family-owned Mediterranean restaurant in Chicago, offering handmade dishes crafted from fresh, locally sourced ingredients. From crisp Greek salads to flavorful bruschetta, every meal reflects tradition and warmth. Join us for an authentic dining experience where great food and hospitality bring people together.  </p>
+      <div className="chicago-content">
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>Little Lemon is a family-owned Mediterranean restaurant in Chicago, offering handmade dishes crafted from fresh, locally sourced ingredients. From crisp Greek salads to flavorful bruschetta, every meal reflects tradition and warmth. Join us for an authentic dining experience where great food and hospitality bring people together.  </p>
+        </div>
     </section>
   );
 }
