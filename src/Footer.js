fbe972fb1ff6c4a9d5f2d79496acaa2128b47314
Paula0M <paulamirazf@gmail.com>
2025-01-30 17:21:32 +0100

    Version1.0

--- src/Footer.js
+++ src/Footer.js
@@ -1,42 +1,51 @@
 import React from 'react';
-import './App.css'; // Import the CSS file for styling'
-import logo from './Assets/logo2.png'; // Import the logo
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'; // Importamos los iconos
+import './App.css'; 
+import logo from './Assets/logo2.png';
 
 function Footer() {
     return (
         <footer className="footer">
             <div className="footer-content">
-            {/* Logo Section */}
-            <div className="footer-logo"> <img src={logo} alt="Little Lemon Logo" />
-            </div>
-            {/* Doormat Navigation */}
-            <div className="footer-nav">
-                <h4>Doormat Navigation</h4>
-                <ul>
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/menu">Menu</a></li>
-                    <li><a href="/reservations">Reservations</a></li>
-                    <li><a href="/order-online">Order Online</a></li>
-                    <li><a href="/login">Login</a></li>
-                </ul>
-            </div>
+                {/* Logo Section */}
+                <div className="footer-logo"> 
+                    <img src={logo} alt="Little Lemon Logo" />
+                </div>
 
-            {/* Contact Section */}
-            <div className="footer-contact">
-                <h4>Contact</h4>
-                <p>Address</p>
-                <p>Phone Number</p>
-                <p>Email</p>
-            </div>
+                {/* Doormat Navigation */}
+                <div className="footer-nav">
+                    <h4>Doormat Navigation</h4>
+                    <ul>
+                        <li><a href="/home">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/menu">Menu</a></li>
+                        <li><a href="/reservations">Reservations</a></li>
+                        <li><a href="/order-online">Order Online</a></li>
+                        <li><a href="/login">Login</a></li>
+                    </ul>
+                </div>
 
-            {/* Social Media Links */}
-            <div className="footer-social">
-                <h4>Social Media Links</h4>
-                <p>Address</p>
-                <p>Phone Number</p>
-                <p>Email</p>
-            </div>
+                {/* Contact Section */}
+                <div className="footer-contact">
+                    <h4>Contact</h4>
+                    <p>Address</p>
+                    <p>Phone Number</p>
+                    <p>Email</p>
+                </div>
+
+                {/* Social Media Links */}
+                <div className="footer-social">
+                    <h4>Social Media</h4>
+                    <div className="social-icons">
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                            <FaFacebook />Facebook  </a>
+                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                            <FaInstagram />Instagram </a>
+                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                            <FaTwitter />
+                            Twitter</a>
+                    </div>
+                </div>
             </div>
         </footer>
     );
--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
-import Logo from './Assets/logo2.png';
+import Logo from './Assets/logo.png';
 
 function Nav() {
   return (
