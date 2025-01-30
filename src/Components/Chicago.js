59a9b4d1f5fc7611799850924a0bda2cb2793f3a
Paula0M <paulamirazf@gmail.com>
2025-01-30 15:41:21 +0100

    Image superposition

--- src/Components/Chicago.js
+++ src/Components/Chicago.js
@@ -8,7 +8,7 @@ function Chicago() {
     <section className="chicago">
       <div className="chicago-content">
         <h2>Little Lemon</h2>
-        <h3>ChicagoSubtítulo</h3>
+        <h3>Chicago</h3>
         <p>Little Lemon is a family-owned Mediterranean restaurant in Chicago, offering handmade dishes crafted from fresh, locally sourced ingredients. From crisp Greek salads to flavorful bruschetta, every meal reflects tradition and warmth. Join us for an authentic dining experience where great food and hospitality bring people together. </p>
         <div className="chicago-images">
           <img src={image1} alt="Imagen 1" />
