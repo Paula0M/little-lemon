dbb456581955364204b0ae7e22535574ff52377a
Paula0M <paulamirazf@gmail.com>
2025-01-30 13:14:50 +0100

    Images added chicago.js

--- src/Components/Chicago.js
+++ src/Components/Chicago.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import '../App.css';
+import image1 from '../Assets/Bruchette.png'; // Asegúrate de ajustar la ruta según sea necesario
+import image2 from '../Assets/LemonDessert.png'; // Asegúrate de ajustar la ruta según sea necesario
 
 function Chicago() {
   return (
     <section className="chicago">
       <div className="chicago-content">
         <h2>Little Lemon</h2>
-        <h3>Chicago</h3>
-        <p>Little Lemon is a family-owned Mediterranean restaurant in Chicago, offering handmade dishes crafted from fresh, locally sourced ingredients. From crisp Greek salads to flavorful bruschetta, every meal reflects tradition and warmth. Join us for an authentic dining experience where great food and hospitality bring people together.  </p>
+        <h3>ChicagoSubtítulo</h3>
+        <p>Little Lemon is a family-owned Mediterranean restaurant in Chicago, offering handmade dishes crafted from fresh, locally sourced ingredients. From crisp Greek salads to flavorful bruschetta, every meal reflects tradition and warmth. Join us for an authentic dining experience where great food and hospitality bring people together. </p>
+        <div className="chicago-images">
+          <img src={image1} alt="Imagen 1" />
+          <img src={image2} alt="Imagen 2" />
         </div>
+      </div>
     </section>
   );
 }
