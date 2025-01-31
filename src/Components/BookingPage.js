b82d9a78450afb1268fd9f90612056835845b94d
Paula0M <paulamirazf@gmail.com>
2025-01-31 13:37:56 +0100

    BookingPage State pass

--- src/Components/BookingPage.js
+++ src/Components/BookingPage.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
+import BookingForm from './BookingForm';
 
 function BookingPage() {
+  // Definir variables de estado para cada campo del formulario
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [guests, setGuests] = useState(1);
+  const [occasion, setOccasion] = useState('');
+  const [availableTimes, setAvailableTimes] = useState([
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00'
+  ]);
+
+  // Manejar cambios en los campos del formulario
+  const handleDateChange = (e) => setDate(e.target.value);
+  const handleTimeChange = (e) => setTime(e.target.value);
+  const handleGuestsChange = (e) => setGuests(e.target.value);
+  const handleOccasionChange = (e) => setOccasion(e.target.value);
+
   return (
     <section className="booking-page">
       <h2>Book a Table</h2>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
-        
-        <label htmlFor="date">Date:</label>
-        <input type="date" id="date" name="date" required />
-        
-        <label htmlFor="time">Time:</label>
-        <input type="time" id="time" name="time" required />
-        
-        <label htmlFor="guests">Number of Guests:</label>
-        <input type="number" id="guests" name="guests" required />
-        
-        <button type="submit">Book Now</button>
-      </form>
+      <BookingForm
+        date={date}
+        time={time}
+        guests={guests}
+        occasion={occasion}
+        availableTimes={availableTimes}
+        handleDateChange={handleDateChange}
+        handleTimeChange={handleTimeChange}
+        handleGuestsChange={handleGuestsChange}
+        handleOccasionChange={handleOccasionChange}
+      />
     </section>
   );
 }
 
-export default BookingPage;
+export default BookingPage; 
