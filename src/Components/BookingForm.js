46dc5b55d3405569188a387ede47d74458396f87
Paula0M <paulamirazf@gmail.com>
2025-01-31 13:33:10 +0100

    Definición de la página de reservars

--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function BookingForm() {
+    //Definir variables de estado para cada campo del formulario
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [guests, setGuests] = useState(1);
+    const [occasion, setOccasion] = useState('');
+    const [availableTimes, setAvailableTimes] = useState([
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00'
+  ]);
+
+  // Manejar cambios en los campos del formulario
+  const handleDateChange = (e) => setDate(e.target.value);
+  const handleTimeChange = (e) => setTime(e.target.value);
+  const handleGuestsChange = (e) => setGuests(e.target.value);
+  const handleOccasionChange = (e) => setOccasion(e.target.value);
+
   return (
     <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}>
       <label htmlFor="res-date">Choose date</label>
-      <input type="date" id="res-date" />
+      <input type="date" id="res-date" value={date} onChange={handleDateChange} />
 
       <label htmlFor="res-time">Choose time</label>
-      <select id="res-time">
-        <option>17:00</option>
-        <option>18:00</option>
-        <option>19:00</option>
-        <option>20:00</option>
-        <option>21:00</option>
+      <select id="res-time" value={time} onChange={handleTimeChange}>
+        {availableTimes.map((time) => (
+          <option key={time} value={time}>{time}</option>
+        ))}
       </select>
 
       <label htmlFor="guests">Number of guests</label>
-      <input type="number" id="guests" min="1" max="10" />
+      <input type="number" id="guests" min="1" max="10" value={guests} onChange={handleGuestsChange} />
 
       <label htmlFor="occasion">Occasion</label>
-      <select id="occasion">
-        <option>Birthday</option>
-        <option>Anniversary</option>
+      <select id="occasion" value={occasion} onChange={handleOccasionChange}>
+        <option value="Birthday">Birthday</option>
+        <option value="Anniversary">Anniversary</option>
       </select>
 
       <input type="submit" value="Make Your Reservation" />
