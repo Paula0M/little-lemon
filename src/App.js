7085e6bd933a7532746a316172bcf399e92d1f5c
Paula0M <paulamirazf@gmail.com>
2025-01-31 11:52:57 +0100

    App.js configurada navegación

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './Components/Home';
+import BookingForm from './Components/BookingForm';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Header />
       <Main />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/booking" component={BookingForm} />
+      </Switch>
     </Router>
   );
 }
