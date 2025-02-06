7e775083101764ebdd7e480f64af59114e908177
Paula0M <paulamirazf@gmail.com>
2025-02-06 11:39:57 +0100

    Bookingform passed from html to jsx

--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -1,7 +1,8 @@
-<form style="display: grid; max-width: 200px; gap: 20px">
-   <label for="res-date">Choose date</label>
-   <input type="date" id="res-date">
-   <label for="res-time">Choose time</label>
+<form style={{display: grid, maxWidth: '200px', gap: '20px'}}>
+
+   <label htmlFor="res-date">Choose date</label>
+   <input type="date" id="res-date" />
+   <label htmlFor="res-time">Choose time</label>
    <select id="res-time ">
       <option>17:00</option>
       <option>18:00</option>
@@ -10,12 +11,12 @@
       <option>21:00</option>
       <option>22:00</option>
    </select>
-   <label for="guests">Number of guests</label>
-   <input type="number" placeholder="1" min="1" max="10" id="guests">
-   <label for="occasion">Occasion</label>
+   <label htmlFor="guests">Number of guests</label>
+   <input type="number" placeholder={1} min={1} max={10} id={guests} />
+   <label htmlFor="occasion">Occasion</label>
    <select id="occasion">
       <option>Birthday</option>
       <option>Anniversary</option>
    </select>
-   <input type="submit" value="Make Your reservation">
+   <input type="submit" defaultValue="Make Your reservation" />
 </form>
