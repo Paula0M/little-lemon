85cdb13c528cd5b62849525d1ae09550541d3222
Paula0M <paulamirazf@gmail.com>
2025-02-07 13:12:15 +0100

    BookingPage updated

--- src/Components/BookingPage.js
+++ src/Components/BookingPage.js
@@ -25,28 +25,20 @@ function BookingPage({ availableTimes }) {
   return (
     <section className="booking-page">
       <h2>Book a Table</h2>
-      <form onSubmit={handleSubmit}>
-
-        <label htmlFor="name">Name:</label>
-        <input type="text" placeholder="name" name="name" value={name} onChange={e => setName(e.target.value)} required />
-        
-        <label htmlFor="date">Date:</label>
-        <input type="date" placeholder="date" name="date" value={date}  onChange={e => setDate(e.target.value)} required />
-        
-        <label htmlFor="time">Time:</label>
-        <input type="time" placeholder="time" name="time" value={time}  onChange={e => setTime(e.target.value)} required />
-        
-        <label htmlFor="guests">Number of Guests:</label>
-        <input type="number" placeholder="guests" name="guests" value={guests} onChange={e => setGuests(e.target.value)} required />
-
-        <label htmlFor='occasion'>Ocassion:</label>
-        <select id='occasion' name='occasion' value={occasion} onChange={e => setOccasion(e.target.value)} required>
-          <option value='Birthday'>Birthday</option>
-          <option value='Anniversary'>Anniversary</option>
-        </select>
-        
-        <button type="submit">Book Now</button>
-      </form>
+      <BookingForm
+        name={name}
+        setName={setName}
+        date={date}
+        setDate={setDate}
+        time={time}
+        setTime={setTime}
+        guests={guests}
+        setGuests={setGuests}
+        occasion={occasion}
+        setOccasion={setOccasion}
+        availableTimes={availableTimes}
+        handleSubmit={handleSubmit}
+      />
     </section>
   );
 }
