6c201ab219467be14b0dc1d5e8f814abdefb4aef
Paula0M <paulamirazf@gmail.com>
2025-02-13 16:24:14 +0100

    disabled button and error message

--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -17,14 +17,26 @@ function BookingForm({
       dispatch({ type: 'UPDATE_TIMES', payload: selectedDate }); // Actualiza los horarios cuando cambie la fecha
     };
 
+  // Función para validar el formulario
+  const isFormValid = () => {
+    return (
+       name.trim() !== '' &&
+       date.trim() !== '' &&
+       time.trim() !== '' &&
+       guests > 0
+    );
+ };
+ 
    return (
      <form onSubmit={handleSubmit} className="booking-form">
        <h2>Book A Table</h2> 
        <label htmlFor="name">Name:</label>
        <input type="text" id="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)} required />
+       {name.trim() === "" && <p className="error-message">Please enter your name.</p>}
  
        <label htmlFor="res-date">Choose date:</label>
        <input type="date" id="res-date" value={date} onChange={handleDateChange} required />
+       {date === "" && <p className="error-message">Please select a date.</p>}
 
  
        <label htmlFor="res-time">Choose time:</label>
@@ -33,9 +45,11 @@ function BookingForm({
            <option key={timeOption} value={timeOption}>{timeOption}</option>
          ))}
        </select>
+       {time === "" && <p className="error-message">Please select a time.</p>}
  
        <label htmlFor="guests">Number of guests:</label>
        <input type="number" id="guests" placeholder="1" min="1" max="10" value={guests} onChange={e => setGuests(e.target.value)} required />
+       {(guests < 1 || guests > 10) && <p className="error-message">Number of guests must be between 1 and 10.</p>}
  
        <label htmlFor="occasion">Occasion:</label>
        <select id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)} required>
@@ -43,7 +57,7 @@ function BookingForm({
          <option value="Anniversary">Anniversary</option>
        </select>
  
-       <button type="submit">Book</button>
+       <button type="submit" disabled={!isFormValid()}>Book</button>
      </form>
    );
  }
