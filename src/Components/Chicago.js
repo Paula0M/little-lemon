b30862dc351d994dd5758952952bbe764fbf7051
Paula0M <paulamirazf@gmail.com>
2025-02-13 17:39:42 +0100

    Chicago accesible

--- src/Components/Chicago.js
+++ src/Components/Chicago.js
@@ -11,13 +11,13 @@ function Chicago() {
         <h3>Chicago</h3>
         <p>Little Lemon is a family-owned Mediterranean restaurant in Chicago, offering handmade dishes crafted from fresh, locally sourced ingredients. From crisp Greek salads to flavorful bruschetta, every meal reflects tradition and warmth. Join us for an authentic dining experience where great food and hospitality bring people together. </p>
         <div className="chicago-images" >
-          <img src={image1} alt="View from the beach of Chicago" aria-describedby="image1-description" />
+          <img src={image1} alt="View from the lake of Chicago" aria-describedby="image1-description" />
             <p id="image1-description" className="visually-hidden">
               A cozy exterior view of Little Lemon restaurant, showcasing its welcoming entrance and Mediterranean-inspired decor.
             </p>
           <img src={image2} alt="Interior view of Little Lemon restaurant" aria-describedby="image2-description" />
             <p id="image2-description" className="visually-hidden">
-            A vibrant interior view of Little Lemon restaurant, highlighting the warm ambiance and traditional Mediterranean design.
+            A breathtaking night view of Chicago's skyline, showcasing the city's iconic skyscrapers illuminated against the dark sky, with the shimmering lights reflecting on the calm waters of Lake Michigan.
             </p>
         </div>
       </div>
