6db9d3920885f7392c0960991718fb890620a103
Paula0M <paulamirazf@gmail.com>
2025-02-13 16:42:21 +0100

    test to pass initial values

--- src/Components/BookingForm.test.js
+++ src/Components/BookingForm.test.js
@@ -1,23 +1,36 @@
-// src/Components/BookingForm.test.js
-import { initializeTimes, updateTimes } from './BookingForm';
-import { fetchAPI } from './api';
+import { render, screen } from '@testing-library/react';
+import BookingForm from './BookingForm';
 
-// Mock the API
-jest.mock('./api', () => ({
-  fetchAPI: jest.fn(() => Promise.resolve(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]))
-}));
+test('El campo de nombre tiene el atributo required', () => {
+  // Valores iniciales para los estados
+  const initialValues = {
+    name: "",
+    date: "",
+    time: "",
+    guests: 1,
+    occasion: "Birthday",
+  };
 
-test("initializeTimes returns the correct available times", async () => {
-  const expectedTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-  const times = await initializeTimes();
-  expect(times).toEqual(expectedTimes);
-});
+  // Renderiza el componente con los valores iniciales
+  render(
+    <BookingForm
+      name={initialValues.name}
+      setName={() => {}}
+      date={initialValues.date}
+      setDate={() => {}}
+      time={initialValues.time}
+      setTime={() => {}}
+      guests={initialValues.guests}
+      setGuests={() => {}}
+      occasion={initialValues.occasion}
+      setOccasion={() => {}}
+      availableTimes={[]}
+      dispatch={() => {}}
+      handleSubmit={() => {}}
+    />
+  );
 
-test("updateTimes updates the times when receiving new times from API", () => {
-  const initialState = ["17:00", "18:00"];
-  const newTimes = ["19:00", "20:00"];
-  const action = { type: 'UPDATE_TIMES', payload: newTimes };
-  
-  const newState = updateTimes(initialState, action);
-  expect(newState).toEqual(newTimes);
+  // Verifica que el campo de nombre tenga el atributo required
+  const nameInput = screen.getByLabelText(/name/i);
+  expect(nameInput).toBeRequired();
 });
