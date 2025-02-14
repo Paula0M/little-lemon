f4c810569bb7f494ee13bc800523d5df837c6f00
Paula0M <paulamirazf@gmail.com>
2025-02-14 12:32:45 +0100

    ConfirmedBooking.js accesibility

--- src/Components/ConfirmedBooking.js
+++ src/Components/ConfirmedBooking.js
@@ -7,17 +7,17 @@ import checkIcon from '../Icons/check.png'
 
 const ConfirmedBooking = () => {
   return (
-    <div className="confirmation-container">
+    <div className="confirmation-container" role="main" aria-label="Booking Confirmation">
       <div className="confirmation-content">
-        <h1>RESERVATION</h1>
-        <div className="confirmation-message">
+      <h1 id="confirmation-heading">RESERVATION</h1>
+        <div className="confirmation-message" aria-live="polite" aria-labelledby="confirmation-heading">
           <h2>Booking Confirmed!</h2>
-          <img src={checkIcon} alt='check' className='section-icon' />
+          <img src={checkIcon} alt='Check mark icon indicating successful booking' className='section-icon' />
           <p>Thank you for your reservation at Little Lemon.</p>
         </div>
       </div>
-    <CustomersSay className="booking-customers" />
-    <Footer />
+    <CustomersSay className="booking-customers" aria-label="Customer Testimonials" />
+    <Footer aria-label="Footer" />
     </div>
   );
 };
