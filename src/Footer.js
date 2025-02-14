4d46d08cd964cdc86121a92f4e4d926595259456
Paula0M <paulamirazf@gmail.com>
2025-02-14 12:26:02 +0100

    Footer.js accesibility

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import logo from './Assets/logo2.png';
 
 function Footer() {
     return (
-        <footer className="footer">
+        <footer className="footer" role="contentinfo" aria-label="Footer"                          >
             <div className="footer-content">
                 {/* Logo Section */}
                 <div className="footer-logo"> 
@@ -13,37 +13,36 @@ function Footer() {
                 </div>
 
                 {/* Doormat Navigation */}
-                <div className="footer-nav">
-                    <h4>Doormat Navigation</h4>
+                <div className="footer-nav" aria-label="Doormat Navigation">
+                <h4 id="doormat-heading">Doormat Navigation</h4>
                     <ul>
-                        <li><a href="/home">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/menu">Menu</a></li>
-                        <li><a href="/reservations">Reservations</a></li>
-                        <li><a href="/order-online">Order Online</a></li>
-                        <li><a href="/login">Login</a></li>
+                        <li><a href="/home" aria-label="Home" tabIndex="0">Home</a></li>
+                        <li><a href="/about" aria-label="About" tabIndex="0">About</a></li>
+                        <li><a href="/menu" aria-label="Menu" tabIndex="0">Menu</a></li>
+                        <li><a href="/reservations" aria-label="Reservations" tabIndex="0">Reservations</a></li>
+                        <li><a href="/order-online" aria-label="Order Online" tabIndex="0">Order Online</a></li>
+                        <li><a href="/login" aria-label="Login" tabIndex="0">Login</a></li>
                     </ul>
                 </div>
 
                 {/* Contact Section */}
-                <div className="footer-contact">
-                    <h4>Contact</h4>
+                <div className="footer-contact" aria-labelledby="contact-heading">
+                    <h4 id="contact-heading">Contact</h4>
                     <p>Address</p>
                     <p>Phone Number</p>
                     <p>Email</p>
                 </div>
 
                 {/* Social Media Links */}
-                <div className="footer-social">
-                    <h4>Social Media</h4>
+                <div className="footer-social" aria-labelledby="social-heading">
+                    <h4 id="social-heading">Social Media</h4>
                     <div className="social-icons">
-                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook" tabIndex="0">
                             <FaFacebook />Facebook  </a>
-                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram" tabIndex="0">
                             <FaInstagram />Instagram </a>
-                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-                            <FaTwitter />
-                            Twitter</a>
+                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter" tabIndex="0">
+                            <FaTwitter />Twitter</a>
                     </div>
                 </div>
             </div>
